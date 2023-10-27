36729629790f31cc8605b49c1f70059f7af26443
Javi Rodriguez <javirro@glue.digital>
2023-10-27 07:49:18 +0200

    Add tooltip for the points

--- src/components/D3/Chart2.tsx
+++ src/components/D3/Chart2.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useMemo, useRef } from 'react';
 import * as d3 from 'd3';
 import '../../App.css'
+
 const ScatterPlot = ({ data }) => {
   const svgRef = useRef();
   const parseDate = d3.timeParse('%Y-%m-%dT%H:%M:%S'); // Date and time format
@@ -17,7 +18,6 @@ const ScatterPlot = ({ data }) => {
       .attr("height", height + margin.top + margin.bottom)
       .append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-      .attr("background", "black")
 
     // Find the minimum and maximum values for both X and Y
     // const xExtent = d3.extent(data, d => d.date);
@@ -46,35 +46,61 @@ const ScatterPlot = ({ data }) => {
       .datum(dataWithParseDate)
       .attr('fill', 'rgba(31, 222, 107, 0.2)')
       .attr('stroke', 'rgba(31, 222, 107, 0.4)')
-      .attr('stroke-width', 2)
+      .attr('stroke-width', 4)
       .attr('d', line)
       .attr('d', area);
 
-    // Create circles for each data point
-    const circles = svg.selectAll('circle')
+
+    // A function that change this tooltip when the user hover a point.
+    // Its opacity is set to 1: we can now see it. Plus it set the text and position of tooltip depending on the datapoint (d)
+    const mouseover = function (event, d) {
+      console.log(event)
+      console.log(d)
+      // Calculate the position for the tooltip
+      const xPosition = xScale(d.date);
+      const yPosition = yScale(d.pnl);
+
+      // Create a tooltip element and set its position and text
+      d3.select(svgRef.current)
+        .append('text')
+        .attr('class', 'tooltip')
+        .attr('x', xPosition)
+        .attr('y', yPosition - 10) // Adjust the vertical position
+        .text(`Date`);
+    }
+
+    // A function that change this tooltip when the leaves a point: just need to set opacity to 0 again
+    const mouseleave = function () {
+      d3.select('.tooltip').remove();
+    }
+
+    svg
+      .append("g")
+      .selectAll("circle")
       .data(dataWithParseDate)
       .enter()
-      .append('circle')
-      .attr("class", "circle")
+      .append("circle")
+      .attr("class", "myCircle")
       .attr('cx', d => xScale(d.date))
       .attr('cy', d => yScale(d.pnl))
-      .attr('r', 3) // Circle radius
-      .attr('fill', d => d.pnl > 0 ? '#1FDE6B' : 'red'); // Set the fill color to green
+      .attr("r", 6)
+      .attr("stroke", "#69b3a2")
+      .attr("stroke-width", 2)
+      .attr('fill', d => d.pnl > 0 ? '#1FDE6B' : 'red') // Set the fill color to green
+      .on("mouseover", mouseover)
+      .on("mouseleave", mouseleave)
 
-    // Add tooltips to the circles
-    circles.append('title')
-      .text(d => `Date: ${d.date.toLocaleString()}, PNL: ${d.pnl}`);
 
     // Optional: Add axes
-    const xAxis = d3.axisBottom(xScale);
+    const xAxis = d3.axisBottom(xScale)
+      .tickSize(0); // Set tickSize to 0 to hide ticks
     // const yAxis = d3.axisLeft(yScale);
 
-    // svg.append('g')
-    //   .attr('transform', `translate(0, ${height})`)
-    //   .call(xAxis);
     svg.append('g')
       .attr('transform', `translate(0, ${yScale(0)})`) // Position X-axis at the zero line
-      .call(xAxis);
+      .call(xAxis)
+      .selectAll('text') // Hide X-axis tick text
+      .remove();
     // svg.append('g')
     //   .call(yAxis);
   }, [dataWithParseDate]);
