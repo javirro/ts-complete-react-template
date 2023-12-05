d46645527e98d8f8c3941407e6eb3c27f2091178
Javier Rodriguez <javirro@glue.digital>
2023-12-05 07:35:49 +0100

    Add generate first echart

--- src/components/Echarts/EchartsChart.tsx
+++ src/components/Echarts/EchartsChart.tsx
@@ -1,8 +1,93 @@
-const EchartsChart = ({ data }) => {
-    console.log(data)
-    return (<section>
-        Hello
-    </section>)
+import React, { useEffect, useRef } from 'react';
+import * as echarts from 'echarts';
+import { PNLData } from 'src/utils/types';
+
+const EchartsChart = ({ data }: { data: PNLData[] }) => {
+  const chartRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (chartRef.current) {
+      // Initialize ECharts instance
+      const myChart = echarts.init(chartRef.current);
+
+      // ECharts options for the area chart
+      const options: echarts.EChartsOption = {
+        tooltip: {
+          trigger: 'axis',
+          position: function (pt) {
+            return [pt[0], '10%'];
+          }
+        },
+        toolbox: {
+          feature: {
+            dataZoom: {
+              yAxisIndex: 'none'
+            },
+            restore: {},
+            saveAsImage: {}
+          }
+        },
+        title: {
+          left: 'center',
+          text: 'Large Scale Area Chart',
+        },
+        xAxis: {
+          type: 'category',
+          boundaryGap: false,
+          data: data.map(d => d.date)
+        },
+        yAxis: {
+          type: 'value',
+          boundaryGap: [0, '100%']
+        },
+        dataZoom: [
+          {
+            type: 'inside',
+            start: 0,
+            end: 10
+          },
+          {
+            start: 0,
+            end: 10
+          }
+        ],
+        series: [
+          {
+            name: 'Pnl',
+            type: 'line',
+            symbol: 'none',
+            sampling: 'lttb',
+            itemStyle: {
+              color: 'rgb(255, 70, 131)'
+            },
+            areaStyle: {
+              color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                {
+                  offset: 0,
+                  color: 'rgb(255, 158, 68)'
+                },
+                {
+                  offset: 1,
+                  color: 'rgb(255, 70, 131)'
+                }
+              ])
+            },
+            data: data.map(d => d.pnl),
+          },
+        ],
+      };
+
+      // Set the options and render the chart
+      myChart.setOption(options);
+
+      // Clean up the chart when the component is unmounted
+      return () => {
+        myChart.dispose();
+      };
+    }
+  }, [data]);
+  return (<section className='echart-section'>
+    <div ref={chartRef} style={{ width: '100%', height: '400px' }} />;
+  </section>)
 }
 
 export default EchartsChart
--- src/utils/dateChartsHelper.tsx
+++ src/utils/dateChartsHelper.tsx
@@ -1,15 +1,34 @@
-import {  PNLData } from "./types"
+import { PNLData } from "./types"
 
 export const formatDatesForGraphicFooter = (pnlData: PNLData): string => {
 
-      const newDate = new Date(pnlData.date)
-      const date =  ({
-        year: newDate.getUTCFullYear(),
-        month: (newDate.getUTCMonth() + 1) < 10 ? "0" + (newDate.getUTCMonth() + 1) : (newDate.getUTCMonth() + 1),
-        day: (newDate.getUTCDate()) < 10 ? "0" + (newDate.getUTCDate()) : (newDate.getUTCDate()),
-        hour: newDate.getUTCHours() < 10 ? "0" + newDate.getUTCHours() : newDate.getUTCHours(),
-        min: newDate.getUTCMinutes() < 10 ? "0" + newDate.getUTCMinutes() : newDate.getUTCMinutes(),
-        sec: newDate.getUTCSeconds() < 10 ? "0" + newDate.getUTCSeconds() : newDate.getUTCSeconds()
-      })
-      return `${date.year}-${date.month}-${date.day} ${date.hour}:${date.min}: ${date.sec}`
+  const newDate = new Date(pnlData.date)
+  const date = ({
+    year: newDate.getUTCFullYear(),
+    month: (newDate.getUTCMonth() + 1) < 10 ? "0" + (newDate.getUTCMonth() + 1) : (newDate.getUTCMonth() + 1),
+    day: (newDate.getUTCDate()) < 10 ? "0" + (newDate.getUTCDate()) : (newDate.getUTCDate()),
+    hour: newDate.getUTCHours() < 10 ? "0" + newDate.getUTCHours() : newDate.getUTCHours(),
+    min: newDate.getUTCMinutes() < 10 ? "0" + newDate.getUTCMinutes() : newDate.getUTCMinutes(),
+  })
+  return `${date.year}-${date.month}-${date.day} ${date.hour}:${date.min}`
+}
+
+// RANDOM CHART DATA
+const generateRandomPnl = () => {
+  return (Math.random() * (20 - (-20)) - 20).toFixed(2);
+};
+
+export const generateDataArray = (): PNLData[] => {
+  const startDate = new Date('2023-01-01T09:30:00');
+  const dataArray = [];
+
+  for (let i = 0; i < 100; i++) {
+    const currentDate = new Date(startDate.getTime() + i * 5 * 60 * 1000);
+    const formattedDate = currentDate.toISOString();
+    const randomPnl = generateRandomPnl();
+
+    dataArray.push({ date: formattedDate, pnl: parseFloat(randomPnl) });
   }
+
+  return dataArray;
+};
